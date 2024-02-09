aa7646f75a3acc3103800358bd756b1b2acce7a7
Niggo2001 <37769599+Niggo2001@users.noreply.github.com>
2024-02-09 14:44:57 +0100

    Created lobbies.services.js, Added API to get data by gameId. @sese317j lets talk about this Code pls. Service APIs Front and Backend #7

---
+++ backend/src/services/lobbies.services.js
@@ -0,0 +1,27 @@
+const db = require('./db.service'); //DB Service Import
+const express = require('express'); //Express Import
+
+const app = express();              //Create Express App
+
+// Endpoint to get lobby data
+app.get('/api/lobbydata/:gameId', (request, response) => {
+    const gameId = request.params.gameId;
+    const sql = `
+        SELECT                                      
+            g.pk_game_id as gameId,
+            gt.pk_game_type_id as gameTypeId,
+            gt.description as gameTypeDescription,
+            g.date as date,
+            g.amount_sets as amountSets,
+            g.amount_legs as amountLegs
+        FROM game g
+        LEFT JOIN game_type gt ON g.fk_game_type_id = gt.pk_game_type_id
+        WHERE g.pk_game_id = ?`;
+    db.query(sql, [gameId], (error, results) => {
+        if (error) {
+            response.status(500).json({message: 'Internal Server Error'});
+            throw error;
+        }
+        response.json(results);
+    });
+});
