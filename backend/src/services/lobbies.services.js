97ac2f93d134eda64c59c5638842d93e4062c579
Niggo2001 <37769599+Niggo2001@users.noreply.github.com>
2024-02-13 22:10:56 +0100

    formatted SQL

--- backend/src/services/lobbies.services.js
+++ backend/src/services/lobbies.services.js
@@ -8,12 +8,12 @@ app.get('/api/lobbydata/:gameId', (request, response) => {
     const gameId = request.params.gameId;
     const sql = `
         SELECT                                      
-            g.pk_game_id as gameId,
-            gt.pk_game_type_id as gameTypeId,
-            gt.description as gameTypeDescription,
-            g.date as date,
-            g.amount_sets as amountSets,
-            g.amount_legs as amountLegs
+            g.pk_game_id as gameId
+            ,gt.pk_game_type_id as gameTypeId
+            ,gt.description as gameTypeDescription
+            ,g.date as date
+            ,g.amount_sets as amountSets
+            ,g.amount_legs as amountLegs
         FROM game g
         LEFT JOIN game_type gt ON g.fk_game_type_id = gt.pk_game_type_id
         WHERE g.pk_game_id = ?`;
